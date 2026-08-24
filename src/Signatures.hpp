6e90505118e95725316aa216189b1f53d1b22be1
tuffxiomi <tuffxiomi@gmail.com>
2026-08-24 16:49:37 +0800

    Update signatures for game and survival modes

--- src/Signatures.hpp
+++ src/Signatures.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <cstdint>
 #include <string_view>
 
 namespace accurate_block_placement::signatures {
 
 inline constexpr std::string_view kGameModeUseItemOn =
-    "? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 ? ? ? A9 FD 03 00 91 "
-    "? ? ? D1 ? ? ? F9 5C D0 3B D5 F8 03 00 AA";
+    "FF 43 01 D1 F5 5B 02 A9 F3 53 03 A9";
 
 inline constexpr std::string_view kSurvivalModeUseItemOn =
-    "? ? ? 39 ? ? ? 34 ? ? ? F0 ? ? ? 39 ? ? ? 34 ? ? ? A9 FD 03 00 91 "
-    "E1 03 1F 2A ? ? ? 97 E0 03 1F 2A ? ? ? A8 C0 03 5F D6 ? ? ? 12";
+    "FF 43 01 D1 F5 5B 02 A9 F3 53 03 A9";
 
 } // namespace accurate_block_placement::signatures
